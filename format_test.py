4dd3822d09c490f444d198ce9daf75b17da0f3e1
Ken <kenliu.liu06@gmail.com>
2019-06-24 20:33:28 +1000

    Initial commit

---
+++ format_test.py
@@ -0,0 +1,18 @@
+# Format holder
+
+# Ctrl & D --> copy and pasting the current line
+# Ctrl & Y --> delete the current line
+# Ctrl & F --> search
+# Ctrl & R --> replace
+# Ctrl & W --> expanding the selection outwards
+# Ctrl & / --> comment/uncomment (select a whole function)
+# Ctrl & - --> compress the function
+# Ctrl & + --> expand the function
+# Ctrl & hover --> checking related info
+# Ctrl & hover & click --> checking related info in detail
+# Tab --> tab
+# Shift & Tab --> undo tab
+# Shift & Enter --> new line from any point
+# Shift & F10 --> running whatever is selected on the top right
+# Ctrl & Shift & F10 --> run the current file
+# ALT & left click --> selecting everything in a column
